0925a0cd9449142c74a3b867bb29d10ca0dd70fc
tytotf08 <joshleeliu@gmail.com>
2020-10-27 09:32:17 -0400

    Wow! It has line numbers and a redesigned token parsing.

--- cli/index.js
+++ cli/index.js
@@ -2,52 +2,69 @@
 // Wowl - a language for Maths.
 'use strict';
 const [, , ...sys_args] = process.argv; // system arguments
+const util = require('util');
 const fs = require('fs'); // Filesystem module.
 const path = require('path');
 // the interpreter will convert the file given into code here when we do that
 const file = path.resolve(sys_args[0]);
+const parseStatement = function(line) {
+  console.log(line);
+  let object = {};
+  let splitStatement = line.split(':');
+  if (splitStatement.length === 2) {
+    object.type = 'variable';
+    object.definition = splitStatement[0];
+    object.value = splitStatement[1]; // 6, x, x+y+6
+  }
+  return object;
+}
 fs.readFile(file, 'utf8', function(err, data) {
   if (err) {
     return console.log(err);
   }
-  const rawCode = data += '\n';
-  let i; // Iterator.
+  const rawCode = data;
 	let ast = []; // Abstract Syntax Tree.
 	let namespace = {}; // NameSpace Object.
-  let statementType;
   let cc = '';
-  let cleanCode = rawCode.replace(/^\#.*\n/gm, '').replace(/\s/g, ''); // remove all whitespace and comments
-  const codeArray = cleanCode.split(';'); // array of statements.
-  if (codeArray.slice(-1)[0] === '') {
-    codeArray.pop(); // remove the empty character at the end.
-  }
+
+  let codeArray = rawCode.split('\n');
   // add to the ast array
-  for (i = 0; i < codeArray.length; i += 1) {
-    let object = {};
-    let splitStatement = codeArray[i].split(':');
-    if (splitStatement.length === 2) {
-      object.type = 'variable';
-      object.definition = splitStatement[0];
-      object.value = splitStatement[1]; // 6, x, x+y+6
-      ast.push(object);
+  for (let i = 1; i < codeArray.length + 1; i += 1) {
+    // does it have a semicolon
+    // is it a variable def
+
+    let line = codeArray[i-1];
+
+    if(line.startsWith('#')){
+      continue;
+    }
+    if(line.replace(/\s/g, '') === ''){
+      continue;
+    }
+    if(!line.endsWith(';')){
+      console.log("All lines must have a semicolon. (line " + i + ')');
+      process.exit();
+    }
+
+    let statements = line.split(';');
+    statements.pop();
+    
+    for(let j=0; j<statements.length; j++){
+      ast.push(parseStatement(statements[j]));
     }
   }
-  // so we gonna now filter through the ast
-  for (i = 0; i < ast.length; i += 1) {
-    if (ast[i].type === 'variable') {
-    	if (String(Number(ast[i].value)) !== 'NaN') {
-    		namespace[ast[i].definition] = ast[i].value;
-      	cc += 'var ' + ast[i].definition + ' = ' + ast[i].value + ';\n'; // add to the compiled code variable!
-      	cc += 'console.log(String(' + ast[i].definition + '));\n'; // node paints numbers strangely
-      } else if (namespace[ast[i].value]) {
-      		namespace[ast[i].definition] = namespace[ast[i].value];
-      		cc += 'var ' + ast[i].definition + ' = ' + namespace[ast[i].value] + ';\n'; // add to the compiled code variable!
-      		cc += 'console.log(String(' + ast[i].definition + '));\n'; // node paints numbers strangely
-      } else {
-      	console.log('Bleh bleh. Error.');
-      }
-    }    	
-  }
-  global.eval(cc);
-});
 
+  // so we gonna now filter through the ast
+  for (let i = 0; i < ast.length; i += 1) {
+    
+    cc += ast[i].definition + ' = ' + ast[i].value + ';\n'; // add to the compiled code letiable!
+    cc += 'console.log(String(' + ast[i].definition + '));\n'; // node paints numbers strangely   	
+  }
+  try {
+    global.eval(cc);
+  } catch(ReferenceError) {
+    console.log('Bleh! Bad, fatal, lethal, error. You referenced a nonexistent letiable!');
+    console.log(ReferenceError);
+    console.log(util.inspect(ReferenceError).split('\n')[3].split(':')[1]);
+  }
+});
